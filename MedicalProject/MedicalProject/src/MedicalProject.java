f640592f38ac98a5313ea2e16502e93475cf5f60
rctx <rctx11@gmail.com>
2014-12-07 18:56:21 -0600

    in progress 2

--- MedicalProject/MedicalProject/src/MedicalProject.java
+++ MedicalProject/MedicalProject/src/MedicalProject.java
@@ -2,6 +2,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.ontology.OntModel;
@@ -26,75 +29,18 @@ import com.hp.hpl.jena.util.FileManager;
 public class MedicalProject {
 
 	public static void main(String[] args) {
-		// Temp values for now, later make variable to needs
-		String[] Nodes = new String[100];
-		String[] Depth = new String[20]; //hold last node# for that depth
-		String[] UserSymptoms = new String[3];
 		
-		//
-		// Change this to relational
-		//
-		String directory = "file:C:\\Users\\Ryan\\Documents\\Med4.owl";
-		SPRQLQuery queryObj = new SPRQLQuery(directory);
+		MedicalProject MP = new MedicalProject();
+		if(true){return;}
+		// OLD TESTING CODE
 		
-		String SymptomList = queryObj.getSymptoms();
-		//System.out.println("SymptomList:" + SymptomList);
-		String[] AllSymptomList = SymptomList.split(" ");
-		
-		// Get input of symptoms from user
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int numSymptoms = 0;
-		while(numSymptoms < 3){
-			System.out.println("Enter Symptom " + (numSymptoms + 1) + " of 3:");
-			String inSymp = "";
-			try {
-				inSymp = br.readLine();
-		      } catch (IOException ioe) {
-		         System.out.println("IO error trying to read your symptom!");
-		         System.exit(1);
-		      }
-			if(inSymp.equals("list")){
-				System.out.println("List of Symptoms: " + SymptomList);
-			}
-			else if(!inSymp.equals("")){
-				// Check if its actually a Symptom
-				boolean realSymptom = false;
-				for(int g = 0; g < AllSymptomList.length; g++){
-					if(AllSymptomList[g].equals(inSymp)){
-						realSymptom = true;
-					}
-				}
-				
-				// If it is then save it
-				if(realSymptom){
-					UserSymptoms[numSymptoms] = inSymp;
-					numSymptoms++;
-				}
-				else{
-					System.out.println("Error trying to read your symptom!");
-					System.out.println("You can enter 'list' to see all symptoms.");
-				}
-			}
-		}// Done gathering input
-		
-		// Create Initial Search Node
-		//SearchNode(int num, int pnum, String unsat, int currCost )
+		//String DiseaseList = queryObj.getDisease("Coughing");
+		//System.out.println("DiseaseList:" + DiseaseList);
 		
 		
-		
-		
-		
-		//SPRQLQuery queryObj = new SPRQLQuery(directory);
-		
-		
-		String DiseaseList = queryObj.getDisease("Coughing");
-		System.out.println("DiseaseList:" + DiseaseList);
-		
-		
-		String DiseaseList2 = queryObj.getDisease("Pain", "Ebola Pneumonia FoodAllergy");
-		System.out.println("DiseaseList2:" + DiseaseList2);
+		//String DiseaseList2 = queryObj.getDisease("Pain", "Ebola Pneumonia FoodAllergy");
+		//System.out.println("DiseaseList2:" + DiseaseList2);
 		if(true) {return;}
-		
 		
 		
 		//Dataset dataset = DatasetFactory.assemble(directory);//,"http://www.semanticweb.org/ryan/ontologies/2014/11/untitled-ontology-2#");
@@ -133,6 +79,175 @@ public class MedicalProject {
 	    } finally {
 	        qexec.close();
 	    }
+	}
+	
+	// Start Input and the A* search
+	public MedicalProject(){
+		
+		int lastCreatedNode = 0;
+		int numInputSymptoms = 3;
+		int stepCost = 3;
+		// Temp values for now, later make variable to needs
+		String[] Nodes = new String[100];
+		String[] Depth = new String[20]; //hold last node# for that depth
+		String[] UserSymptoms = new String[numInputSymptoms];
+		SearchNode[] NodeQueue = new SearchNode[400]; // Sorted by estimated goal cost
+		
+		String[] getDiseases = new String[numInputSymptoms];
+		
+		//
+		// Change this to relational
+		//
+		String directory = "file:C:\\Users\\Ryan\\Documents\\Med4.owl";
+		SPRQLQuery queryObj = new SPRQLQuery(directory);
+		
+		String SymptomList = queryObj.getSymptoms();
+		//System.out.println("SymptomList:" + SymptomList);
+		String[] AllSymptomList = SymptomList.split(" ");
+		
+		// Get input of symptoms from user
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int numSymptoms = 0;
+		while(numSymptoms < numInputSymptoms){
+			System.out.println("Enter Symptom " + (numSymptoms + 1) + " of " + numInputSymptoms + ":");
+			String inSymp = "";
+			try {
+				inSymp = br.readLine();
+		      } catch (IOException ioe) {
+		         System.out.println("IO error trying to read your symptom!");
+		         System.exit(1);
+		      }
+			if(inSymp.equals("list")){
+				System.out.println("List of Symptoms: " + SymptomList);
+			}
+			else if(!inSymp.equals("")){
+				// Check if its actually a Symptom
+				boolean realSymptom = false;
+				for(int g = 0; g < AllSymptomList.length; g++){
+					if(AllSymptomList[g].equals(inSymp)){
+						realSymptom = true;
+					}
+				}
+				
+				// If it is then save it
+				if(realSymptom){
+					UserSymptoms[numSymptoms] = inSymp;
+					numSymptoms++;
+				}
+				else{
+					System.out.println("Error trying to read your symptom!");
+					System.out.println("You can enter 'list' to see all symptoms.");
+				}
+			}
+		}// Done gathering input
+		
+		// Create Initial Search Node
+		//SearchNode(int num, int pnum, String unsat, int currCost, int estimate )
+		String SymptomStr = "";
+		for(int y = 0; y < numInputSymptoms;y++){
+			SymptomStr += UserSymptoms[y] + " ";
+		}
+		SearchNode initialNode = new SearchNode(0, -1, SymptomStr, 0, 0, "none", "none");
+		lastCreatedNode = 0;
+		NodeQueue[lastCreatedNode] = initialNode;
+		//Testing
+		//initialNode = new SearchNode(0, -1, SymptomStr, 0, 0);
+		//lastCreatedNode++;
+		//NodeQueue[lastCreatedNode] = initialNode;
+		//End Testing
+		
+		Arrays.sort(NodeQueue,0, lastCreatedNode, new Comparator<SearchNode>() {
+			   public int compare(SearchNode n1, SearchNode n2) {
+				   if(n1.estimatedGoalCost > n1.estimatedGoalCost){
+					   return 1;
+				   }
+				   if(n1.estimatedGoalCost < n1.estimatedGoalCost){
+					   return -1;
+				   }
+				   else{					   
+					   return 0;
+				   }
+			   }
+		});
+		
+		//SPRQLQuery queryObj = new SPRQLQuery(directory);
+		
+		// Get all Initial diseases with at least one user symptom
+		for(int g = 0; g < numInputSymptoms; g++){
+			String DiseaseL = queryObj.getDisease(UserSymptoms[g]);
+			getDiseases[g] = DiseaseL;
+		}
+		//Create first set of nodes with all diseases with at least one symptom
+		for(int symptomNum = 0; symptomNum < numInputSymptoms; symptomNum++){
+			String[] splitDisease = getDiseases[symptomNum].split(" ");
+			for(int newNode = 0; newNode < splitDisease.length; newNode++){
+				lastCreatedNode++;
+				//Create list of remaining unsatisfied symptoms
+				String symplist = "";
+				String thisSymp = "none";
+				for(int k = 0; k < numInputSymptoms; k++){
+					if(k == symptomNum){
+						thisSymp = UserSymptoms[k];
+						continue;
+					}
+					symplist+= UserSymptoms[k] + " ";
+				}
+				// Get Estimate
+				int estimate = stepCost;
+				String[] getSeverity = splitDisease[newNode].split(":");
+				if(getSeverity[1].equals("has_symptom_strong")){
+					estimate = 1;
+				}
+				if(getSeverity[1].equals("has_symptom_medium")){
+					estimate = 2;
+				}
+				if(getSeverity[1].equals("has_symptom_weak")){
+					estimate = 3;
+				}
+				
+				//SearchNode(int num, int pnum, String unsat, int currCost, int estimate, String Symptom, String Disease)
+				SearchNode SN = new SearchNode(lastCreatedNode, 0, symplist, 1, estimate, thisSymp, getSeverity[0]);
+				NodeQueue[lastCreatedNode] = SN;
+				NodeQueue = sortQueue(NodeQueue);
+			}
+		}
+		
+	}
+	
+	public void printArray(String[] printThis){
+		System.out.println("Printing new array");
+		for(int i = 0; i < printThis.length;i++){
+			System.out.println(" - " + printThis[i]);
+		}
+	}
+	
+	// Remove null(removed) nodes and sort by estimatedGoalCost
+	public SearchNode[] sortQueue(SearchNode[] searchNodes){
+		
+		SearchNode[] retNodes = new SearchNode[searchNodes.length];
+		int num = 0;
+		for(int i = 0; i < searchNodes.length; i++){
+			if(searchNodes[i] == null){continue;}
+			else{
+				retNodes[num] = searchNodes[i];
+				num++;
+			}
+		}
+		Arrays.sort(retNodes ,0, num, new Comparator<SearchNode>() {
+			   public int compare(SearchNode n1, SearchNode n2) {
+				   if(n1.estimatedGoalCost > n1.estimatedGoalCost){
+					   return 1;
+				   }
+				   if(n1.estimatedGoalCost < n1.estimatedGoalCost){
+					   return -1;
+				   }
+				   else{					   
+					   return 0;
+				   }
+			   }
+		});
+		
+		return retNodes;
 	}
 
 }
--- MedicalProject/MedicalProject/src/SearchNode.java
+++ MedicalProject/MedicalProject/src/SearchNode.java
@@ -10,15 +10,22 @@ public class SearchNode {
 	int number;
 	int parentNumber;
 	int totalCost;
+	int estimatedGoalCost;
+	String DiseaseName;
 	String childNodes;// Separated by spaces
 	String[] unsatisfiedSymptom;// = new String[6];
 	String[] satisfiedSymptom;// = new String[6];
+	String Symptom;
 	
-	public SearchNode(int num, int pnum, String unsat, int currCost ){
+	public SearchNode(int num, int pnum, String unsat, int currCost, int estimate, String symp, String Dis){
 		number = num;
 		parentNumber = pnum;
 		unsatisfiedSymptom = unsat.split(" ");
 		totalCost = currCost;
+		estimatedGoalCost = estimate;
+		Symptom = symp;
+		DiseaseName = Dis;
+		System.out.println("Created Node: " + number + " with disease: " + Dis + " with symptom: " + symp + " with parent: " + parentNumber + " with current cost: " + totalCost + " and unsatisfied Symptoms: " + unsat);
 	}
 		
 }
